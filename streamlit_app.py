089df4e516fc7e5d3032ff28960fc020d5a7404f
MormoSantiago <santiagomormo@unisabana.edu.co>
2024-10-28 18:29:55 -0500

    Preguntas

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,53 +37,79 @@ num_cols = ['Activ_Econ', 'Ventas_Nacion19', 'Export_2019', 'Ventas_Nacion20',
             'Export_2020', 'Bienes_Ctes', 'Razon_No_Proy', 'Average_Cert_Employ19', 
             'Average_Cert_Employ20']
 
-
 # Preguntas para los datos numéricos
 numerical_inputs = [
-    ("Activ_Econ", "Código de la actividad económica principal de la empresa", 0),
-    ("Ventas_Nacion19", "Ingresos/ventas totales en el mercado nacional 2019 (en miles de pesos)", 0),
-    ("Export_2019", "Exportaciones totales de la empresa en 2019 (en miles de pesos)", 0),
-    ("Ventas_Nacion20", "Ingresos/ventas totales en el mercado nacional 2020 (en miles de pesos)", 0),
-    ("Export_2020", "Exportaciones totales de la empresa en 2020 (en miles de pesos)", 0),
-    ("Bienes_Ctes", "Porcentaje de ventas nacionales de bienes o servicios que no cambiaron (0 a 1)", 0.0, 0.0, 1.0),
-    ("Razon_No_Proy", "Razón principal por la cual no introdujo innovaciones (1 a 12)", 1, 1, 12),
-    ("Average_Cert_Employ19", "Empleados con certificaciones laborales en 2019", 0),
-    ("Average_Cert_Employ20", "Empleados con certificaciones laborales en 2020", 0)
+    ("Activ_Econ", "Código de la actividad económica principal de la empresa según la clasificación internacional (CIIU)", 0),
+    ("Ventas_Nacion19", "Ingresos/ventas totales en el mercado nacional del año anterior (en miles de pesos)", 0),
+    ("Export_2019", "Exportaciones totales de la empresa en el año anterior (en miles de pesos)", 0),
+    ("Ventas_Nacion20", "Utilidades totales en el mercado nacional del año anterior (en miles de pesos)", 0),
+    ("Export_2020", "Exportaciones totales de la empresa en el año actual (en miles de pesos)", 0),
+    ("Bienes_Ctes", "Porcentaje de ventas nacionales de bienes o servicios que no cambiaron al año anterior (Valores entre 0 y 1)", 0.0, 0.0, 1.0),
+    ("Razon_No_Proy", "Razón principal por la cual no introdujo innovaciones", 1, 1, 12),
+    ("Average_Cert_Employ19", "Empleados con certificaciones laborales en el año anterior", 0),
+    ("Average_Cert_Employ20", "Empleados con certificaciones laborales en el año actual", 0)
 ]
+
+# Opciones para la razón de no innovación
+razon_options = {
+    1: "No hay una razón convincente para innovar",
+    2: "No fue necesario innovar debido a innovaciones realizadas en periodos anteriores",
+    3: "No fue necesario innovar debido a poca competencia en el mercado",
+    4: "Falta de ideas para introducir innovaciones",
+    5: "La empresa tuvo prioridades diferentes a la innovación",
+    6: "Suposición de que la innovación cuesta demasiado",
+    7: "Falta de personal calificado para realizar una innovación",
+    8: "Falta de comprensión del concepto de innovación",
+    9: "Falta de información disponible sobre metodología para realizar una innovación",
+    10: "No fue claro identificar las necesidades de innovación",
+    11: "No hay incentivos a innovar debido a demasiada competencia en el mercado",
+    12: "No se cuenta con infraestructura para desarrollar una innovación"
+}
+
+# Diccionario para mapear respuestas de texto a códigos
+respuesta_map = {
+    'Estricta': 'ESTRIC',
+    'Amplia': 'AMPLIA',
+    'Potencial': 'POTENC',
+    'No innovadora': 'NOINNO',
+    'Internacional': 'INTENC'
+}
 
 # Preguntas personalizadas para datos categóricos con restricciones de respuesta (Sí=1, No=2)
 categorical_questions_restricted = {
-    "Bienes_Nuev_Emp": "¿Introdujo su empresa bienes o servicios nuevos que ya existían en el mercado nacional o internacional, pero eran nuevos para la empresa durante el período 2019-2020? (Sí=1, No=2)",
-    "Bienes_Nuev_Nacion": "¿Introdujo su empresa bienes o servicios nuevos en el mercado nacional durante el período 2019-2020? (Sí=1, No=2)",
-    "Bienes_Nuev_Inter": "¿Introdujo su empresa bienes o servicios nuevos en el mercado internacional durante el período 2019-2020? (Sí=1, No=2)",
-    "Bienes_Mejor_Emp": "¿Introdujo su empresa mejoras a bienes o servicios que ya existían en el mercado, pero que fueron mejorados solo para la empresa? (Sí=1, No=2)",
-    "Bienes_Mejor_Nacion": "¿Introdujo su empresa mejoras a bienes o servicios en el mercado nacional durante el período 2019-2020? (Sí=1, No=2)",
-    "Bienes_Mejor_Inter": "¿Introdujo su empresa mejoras a bienes o servicios en el mercado internacional durante el período 2019-2020? (Sí=1, No=2)",
-    "Metod_Nuev_Prod": "¿Introdujo su empresa métodos nuevos o mejorados de producción de bienes o prestación de servicios durante el período 2019-2020? (Sí=1, No=2)",
-    "Metod_Nuev_Emp": "¿Implementó su empresa métodos organizativos nuevos o mejorados en su funcionamiento interno durante el período 2019-2020? (Sí=1, No=2)",
-    "Tec_Comerce_Nuev": "¿Introdujo su empresa técnicas de comercialización nuevas o mejoradas durante el período 2019-2020? (Sí=1, No=2)",
-    "Metod_Nuev_Dist": "¿Implementó su empresa métodos nuevos o mejorados de distribución, entrega o logística durante el período 2019-2020? (Sí=1, No=2)",
-    "Metod_Nuev_Info": "¿Introdujo su empresa métodos nuevos o mejorados de procesamiento de información o comunicación durante el período 2019-2020? (Sí=1, No=2)",
-    "Metod_Nuev_Conta": "¿Introdujo su empresa métodos nuevos o mejorados para la contabilidad u operaciones administrativas durante el período 2019-2020? (Sí=1, No=2)",
-    "Proy_Bienes_Nuev": "¿Tenía su empresa algún proyecto en marcha para introducir bienes o servicios nuevos o mejorados al finalizar 2020? (Sí=1, No=2)",
-    "Abandono_Proy": "¿Abandonó su empresa algún proyecto de innovación durante el período 2019-2020? (Sí=1, No=2)",
-    "Intencion_Proy": "¿Tuvo su empresa la intención de realizar algún proyecto de innovación durante el período 2019-2020? (Sí=1, No=2)",
-    "Cert_Quali_Process": "¿Obtuvo su empresa certificaciones de calidad de procesos durante el período 2019-2020? (Sí=1, No=2)",
-    "Cert_Quali_Product": "¿Obtuvo su empresa certificaciones de calidad de productos durante el período 2019-2020? (Sí=1, No=2)",
-    "Reglamento": "¿Estuvieron los bienes o servicios de su empresa sujetos a reglamentos técnicos durante el período 2019-2020? (Sí=1, No=2)"
+    "Bienes_Nuev_Emp": "¿Introdujo su empresa bienes o servicios nuevos que ya existían en el mercado nacional o internacional, pero eran nuevos para la empresa durante los 2 últimos años? (Sí=1, No=2)",
+    "Bienes_Nuev_Nacion": "¿Introdujo su empresa bienes o servicios nuevos en el mercado nacional durante los 2 últimos años? (Sí=1, No=2)",
+    "Bienes_Nuev_Inter": "¿Introdujo su empresa bienes o servicios nuevos en el mercado internacional durante los 2 últimos años? (Sí=1, No=2)",
+    "Bienes_Mejor_Emp": "¿Introdujo su empresa mejoras a bienes o servicios que ya existían en el mercado, pero que fueron mejorados solo para la empresa durante los 2 últimos años? (Sí=1, No=2)",
+    "Bienes_Mejor_Nacion": "¿Introdujo su empresa mejoras a bienes o servicios en el mercado nacional durante los 2 últimos años? (Sí=1, No=2)",
+    "Bienes_Mejor_Inter": "¿Introdujo su empresa mejoras a bienes o servicios en el mercado internacional durante los 2 últimos años? (Sí=1, No=2)",
+    "Metod_Nuev_Prod": "¿Introdujo su empresa métodos nuevos o mejorados de producción de bienes o prestación de servicios durante los 2 últimos años? (Sí=1, No=2)",
+    "Metod_Nuev_Emp": "¿Implementó su empresa métodos organizativos nuevos o mejorados en su funcionamiento interno durante los 2 últimos años? (Sí=1, No=2)",
+    "Tec_Comerce_Nuev": "¿Introdujo su empresa técnicas de comercialización nuevas o mejoradas durante los 2 últimos años? (Sí=1, No=2)",
+    "Metod_Nuev_Dist": "¿Implementó su empresa métodos nuevos o mejorados de distribución, entrega o logística durante los 2 últimos años? (Sí=1, No=2)",
+    "Metod_Nuev_Info": "¿Introdujo su empresa métodos nuevos o mejorados de procesamiento de información o comunicación durante los 2 últimos años? (Sí=1, No=2)",
+    "Metod_Nuev_Conta": "¿Introdujo su empresa métodos nuevos o mejorados para la contabilidad u operaciones administrativas durante los 2 últimos años? (Sí=1, No=2)",
+    "Proy_Bienes_Nuev": "¿Tenía su empresa algún proyecto en marcha para introducir bienes o servicios nuevos o mejorados al finalizar los 2 últimos años? (Sí=1, No=2)",
+    "Abandono_Proy": "¿Abandonó su empresa algún proyecto de innovación durante los 2 últimos años? (Sí=1, No=2)",
+    "Intencion_Proy": "¿Tuvo su empresa la intención de realizar algún proyecto de innovación durante los 2 últimos años? (Sí=1, No=2)",
+    "Cert_Quali_Process": "¿Obtuvo su empresa certificaciones de calidad de procesos durante los 2 últimos años? (Sí=1, No=2)",
+    "Cert_Quali_Product": "¿Obtuvo su empresa certificaciones de calidad de productos durante los 2 últimos años? (Sí=1, No=2)",
+    "Reglamento": "¿Estuvieron los bienes o servicios de su empresa sujetos a reglamentos técnicos durante los 2 últimos años? (Sí=1, No=2)"
 }
 
 # Definir preguntas categóricas sin restricciones
 categorical_inputs_unrestricted = {
-    "Tipo": "Seleccione el tipo de empresa",
-    "Ventas_NacionTotal": "Porcentaje de ventas nacionales de bienes o servicios totales (0 a 1)"
+    "Tipo": "Seleccione el tipo de empresa según el grado de innovación",
+    "Ventas_NacionTotal": "Porcentaje de ventas nacionales de bienes o servicios totales en el año actual"
 }
 
 # Función para obtener entradas numéricas
 def get_numerical_input():
     data = {}
     for col, question, default, *limits in numerical_inputs:
-        if limits:  # Casos donde hay un rango de valores
+        if col == "Razon_No_Proy":  # Cambia a selectbox para esta entrada
+            data[col] = st.radio(question, options=list(razon_options.keys()), format_func=lambda x: razon_options[x])
+        elif limits:  # Casos donde hay un rango de valores
             data[col] = st.number_input(question, min_value=limits[0], max_value=limits[1], value=default)
         else:
             data[col] = st.number_input(question, value=default)
@@ -93,27 +119,36 @@ def get_numerical_input():
 def get_categorical_input_restricted():
     data = {}
     for col, question in categorical_questions_restricted.items():
-        data[col] = st.selectbox(question, options=[1, 2])
+        data[col] = st.radio(question, options=[1, 2])
     return data
 
-# Función para obtener entradas categóricas sin restricciones
 def get_categorical_input_unrestricted():
     data = {}
-    for col, question in categorical_inputs_unrestricted.items():
-        if col == 'Tipo':
-            data[col] = st.selectbox(question, options=['AMPLIA', 'NOINNO', 'POTENC', 'INTENC', 'ESTRIC'])
-        elif col == 'Ventas_NacionTotal':
-            data[col] = st.number_input(question, min_value=0.0, max_value=1.0, value=0.0)
+    
+    # Obtener opciones para el selectbox desde respuesta_map
+    tipo_options = list(respuesta_map.keys())  # Opciones para el selectbox
+    data["Tipo"] = st.radio(categorical_inputs_unrestricted["Tipo"], options=tipo_options)
+    
+    # Entrada numérica para ventas nacionales totales
+    data["Ventas_NacionTotal"] = st.number_input(
+        categorical_inputs_unrestricted["Ventas_NacionTotal"],
+        min_value=0.0,
+        max_value=1.0,
+        value=0.0
+    )
+    
     return data
 
-# Recolectar los datos de entrada
+
+
+# Obtener entradas del usuario
 numerical_data = get_numerical_input()
 categorical_data_restricted = get_categorical_input_restricted()
 categorical_data_unrestricted = get_categorical_input_unrestricted()
 
-# Unir todos los datos en un solo DataFrame
+# Concatenar todos los datos
 input_data = {**numerical_data, **categorical_data_restricted, **categorical_data_unrestricted}
-input_df = pd.DataFrame([input_data])
+input_df = pd.DataFrame([input_data])  # Convertir a DataFrame
 
 if 'porcentaje_cambio' not in training_data.columns:
     training_data['porcentaje_cambio'] = np.nan  # Add as NaN if not prese
